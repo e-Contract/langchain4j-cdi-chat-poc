76b2ffdc508741052af2cc60d0fe32116f2101ec
Frank Cornelis <info@e-contract.be>
2025-11-30 12:02:21 +0100

    nice interface

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Proof-of-concept for CDI based chat scope
 
+What do we want to achieve?
+* using CDI events instead of direct usage of `TokenStream`. This way the Jakarta EE "model", where the AI code lives, has a way to communicate towards the frond-end code.
+* introduce a `@ChatScoped` CDI scope that corresponds with the lifecycle of the chat session.
+* ensure that all fired CDI events and tool invocations run within the right Jakarta EE context.
+
+## Integration Tests
+
 Running the integration tests requires a local running WildFly.
 
 Run the integration tests via:
@@ -7,4 +14,4 @@ Run the integration tests via:
 mvn clean test -Pintegration-tests
 ```
 
-You might need to change the `StreamingChatModel` configuration with `ChatTestBean.java`.
+You might need to change the `StreamingChatModel` configuration within `ChatTestBean.java`.
---
+++ src/test/java/test/integ/be/e_contract/ai/arquillian/chat/impl/ChatServiceImpl.java
@@ -0,0 +1,81 @@
+package test.integ.be.e_contract.ai.arquillian.chat.impl;
+
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.service.TokenStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.concurrent.ContextService;
+import jakarta.enterprise.concurrent.ManagedExecutorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import test.integ.be.e_contract.ai.arquillian.chat.ChatErrorEvent;
+import test.integ.be.e_contract.ai.arquillian.chat.ChatService;
+import test.integ.be.e_contract.ai.arquillian.chat.CompleteResponseEvent;
+import test.integ.be.e_contract.ai.arquillian.chat.PartialResponseEvent;
+import test.integ.be.e_contract.ai.arquillian.chat.StartChatScopeEvent;
+import test.integ.be.e_contract.ai.arquillian.chat.StopChatScopeEvent;
+
+public class ChatServiceImpl implements ChatService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatServiceImpl.class);
+
+    @Resource
+    private ManagedExecutorService managedExecutorService;
+
+    @Resource
+    private ContextService contextService;
+
+    @Override
+    public String decorate(TokenStream tokenStream) {
+        String identifier = UUID.randomUUID().toString();
+        FireEventContext fireEventContext = this.contextService.createContextualProxy(new FireEventContextImpl(),
+                FireEventContext.class);
+        fireEventContext.fire(new StartChatScopeEvent(identifier, this.contextService));
+        tokenStream
+                .onPartialResponse((String partialResponse) -> {
+                    try {
+                        this.managedExecutorService.submit(() -> {
+                            fireEventContext.fire(new StartChatScopeEvent(identifier));
+                            fireEventContext.fire(new PartialResponseEvent(identifier, partialResponse));
+                        }).get();
+                    } catch (InterruptedException | ExecutionException ex) {
+                        LOGGER.error("error: " + ex.getMessage(), ex);
+                    }
+                })
+                .onIntermediateResponse((ChatResponse chatResponse) -> {
+                    // next is required for the tool invocations
+                    // so it has to be fired on this thread
+                    fireEventContext.fire(new StartChatScopeEvent(identifier));
+                })
+                .onCompleteResponse((ChatResponse chatResponse) -> {
+                    try {
+                        this.managedExecutorService.submit(() -> {
+                            fireEventContext.fire(new StartChatScopeEvent(identifier));
+                            CompleteResponseEvent event = new CompleteResponseEvent(identifier, chatResponse);
+                            fireEventContext.fire(event);
+                            if (event.isEndChatScope()) {
+                                fireEventContext.fire(new StopChatScopeEvent(identifier));
+                            }
+                        }).get();
+                    } catch (InterruptedException | ExecutionException ex) {
+                        LOGGER.error("error: " + ex.getMessage(), ex);
+                    }
+                })
+                .onError((Throwable throwable) -> {
+                    LOGGER.error("error: " + throwable.getMessage(), throwable);
+                    try {
+                        this.managedExecutorService.submit(() -> {
+                            ChatErrorEvent event = new ChatErrorEvent(identifier, throwable);
+                            fireEventContext.fire(event);
+                            if (event.isEndChatScope()) {
+                                fireEventContext.fire(new StopChatScopeEvent(identifier));
+                            }
+                        }).get();
+                    } catch (InterruptedException | ExecutionException ex) {
+                        LOGGER.error("error: " + ex.getMessage(), ex);
+                    }
+                });
+        return identifier;
+    }
+}
