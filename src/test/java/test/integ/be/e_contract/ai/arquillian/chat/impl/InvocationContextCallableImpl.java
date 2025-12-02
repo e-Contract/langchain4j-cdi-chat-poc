97c2a394edb4081b6904889b0610009bad517ce7
Frank Cornelis <info@e-contract.be>
2025-12-02 09:36:04 +0100

    improve transaction handling

--- src/test/java/test/integ/be/e_contract/ai/arquillian/chat/impl/InvocationContextCallableImpl.java
+++ src/test/java/test/integ/be/e_contract/ai/arquillian/chat/impl/InvocationContextCallableImpl.java
@@ -3,9 +3,13 @@ package test.integ.be.e_contract.ai.arquillian.chat.impl;
 import jakarta.enterprise.inject.spi.CDI;
 import jakarta.interceptor.InvocationContext;
 import jakarta.transaction.UserTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import test.integ.be.e_contract.ai.arquillian.chat.StartChatScopeEvent;
 
 public class InvocationContextCallableImpl implements InvocationContextCallable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InvocationContextCallableImpl.class);
 
     private InvocationContext invocationContext;
 
@@ -32,9 +36,13 @@ public class InvocationContextCallableImpl implements InvocationContextCallable 
         }
         this.userTransaction.begin();
         try {
-            return this.invocationContext.proceed();
-        } finally {
+            Object result = this.invocationContext.proceed();
             this.userTransaction.commit();
+            return result;
+        } catch (Exception t) {
+            LOGGER.error("error: " + t.getMessage(), t);
+            this.userTransaction.rollback();
+            throw t;
         }
     }
 }
